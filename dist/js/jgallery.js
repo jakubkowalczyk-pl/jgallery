a53b21a39868aa0f506097bdd6b302c2725a7923
Alessandro Rossi <alexred@alexred.com>
2015-02-03 10:43:09 +0000

    Update jgallery.js
    Preload images next previous

--- dist/js/jgallery.js
+++ dist/js/jgallery.js
@@ -1884,6 +1884,25 @@ var Zoom = ( function( jLoader, overlay, historyPushState, jGalleryTransitions, 
             var albumTitle;
             var transition;
             var transitionName;
+            
+            //preload images next prev
+            var $nexta=$a.next();
+            if ($nexta.length>0){
+                //this.jGallery.preloadNext = new Image();
+                //this.jGallery.preloadNext.src=$nexta.attr( 'href' );
+                if ( ! self.isLoaded( $nexta ) ) {
+                    this.appendPhoto( $nexta );
+                }
+            }
+            var $preva=$a.prev();
+            if ($preva.length>0){
+                //this.jGallery.preloadPrev = new Image();
+                //this.jGallery.preloadPrev.src=$preva.attr( 'href' );
+                if ( ! self.isLoaded( $preva ) ) {
+                    this.appendPhoto( $preva );
+                }
+            }
+            //preload images next prev
 
             if ( ! this.jGallery.initialized() ) {
                 this.showPhotoInit();
